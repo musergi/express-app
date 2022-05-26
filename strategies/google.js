cb742a5ce862a7ea081c5313ce772ca80ee8972b
Sergi Mus <sergi.mus@gmail.com>
2022-05-26 15:40:34 +0200

    Google oidc not working

--- strategies/google.js
+++ strategies/google.js
@@ -1,8 +1,10 @@
-const Strategy = require('passport-openidconnect');
+const Strategy = require('passport-google-oidc');
 
 function parseUser(issuer, profile, done)
 {
-    const user = { username: profile.displayName, provider: issuer };
+    console.log(issuer);
+    console.log(profile);
+    const user = { username: profile.displayName, provider: 'Google' };
     done(null, user);
 }
 
@@ -11,10 +13,6 @@ function GoogleStrategy(callback)
     if (!process.env.GOOGLE_OAUTH_CLIENT_ID || !process.env.GOOGLE_OAUTH_SECRET)
         throw Error("GOOGLE_OAUTH_CLIENT_ID and GOOGLE_OAUTH_SECRET must be defined");
     const config = {
-        issuer: 'https://accounts.google.com',
-        authorizationURL: 'https://accounts.google.com/o/oauth2/v2/auth',
-        tokenURL: 'https://oauth2.googleapis.com/token',
-        userInfoURL: 'https://openidconnect.googleapis.com/v1/userinfo',
         clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
         clientSecret: process.env.GOOGLE_OAUTH_SECRET,
         callbackURL: callback,
